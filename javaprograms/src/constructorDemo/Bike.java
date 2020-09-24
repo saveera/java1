9b38df6366072243bde0d03d642fdafc93358613
ds_ku <ds_ku@DESKTOP-I24MAUC>
2020-09-24 19:15:57 -0400

    commit

---
+++ javaprograms/src/constructorDemo/Bike.java
@@ -0,0 +1,16 @@
+package constructorDemo;
+
+public class Bike {
+
+	Bike()
+	{
+		System.out.println("Bike Constructor is called");
+	}
+	
+	public static void main(String[] args) {
+		
+		Bike b = new Bike();
+
+	}
+
+}
